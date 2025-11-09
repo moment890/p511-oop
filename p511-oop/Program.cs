a055cc0fa44a009d79d9f201a0969e8aae311e6d
StudentKA <StudentKA@WIN-H77MA6T5FBT>
2025-11-09 14:17:16 +0500

    Добавьте файлы проекта.

---
+++ p511-oop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p511_oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
