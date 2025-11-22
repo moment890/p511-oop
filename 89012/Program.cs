4b0aa311736bfeab3f42f4f0ae4ef833740a5855
Student <Student@213-11>
2025-11-22 13:17:04 +0500

    Добавьте файлы проекта.

---
+++ 89012/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _89012
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
