e5cf54175b6e06c298710a453424a189e552b041
moment890 <akostromin2288@gmail.com>
2025-11-22 14:36:56 +0500

    8_exercise

--- p511-oop/Program.cs
+++ p511-oop/Program.cs
@@ -4,46 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace p511_oop {
-    class User {
-        public string name;
-        public string surname;
-        public int age;
-        public string city;
-
-
-
-        public User(string name , string surname , int age , string city) 
-        {
-         this.name = name;
-         this.surname = surname;
-         this.age = age;
-         this.city = city;
-
-        } 
-
-
-        public void print() 
-        {
-            Console .WriteLine( $"Name:{name}, Surname: {surname}, Age: {age}, city: {city}");
-        }
-
-            
-        
-
-        }
-
-
+namespace p511_oop
+{
+  
     internal class Program
     {
+        static bool IsPalindrome(string str) {
+            // Приводим строку к нижнему регистру и удаляем пробелы  
+            str = str.ToLower().Replace(" ", "");
+
+            // Используем два указателя для сравнения символов  
+            int left = 0;
+            int right = str.Length - 1;
+
+            while (left < right) {
+                if (str[left] != str[right]) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+
+            return true;
+        }  
+
         static void Main(string[] args)
         {
-
-            User user = new User("bob" , "doe" , 25 ,"tyumen" );
-
-            user.print();
+            // Запрос ввода строки у пользователя 
+            Console.Write("Введите строку для проверки: ");
+            string input = Console.ReadLine();
+            // Проверка на палиндром 
+            if (IsPalindrome(input)) {
+                Console.WriteLine("Введённая строка является палиндромом!");
+            } else {
+                Console.WriteLine("Введённая строка не является палиндромом.");
+            }
 
             Console.ReadKey();
+
+
+
         }
     }
 }
