ce43b17fb18f9ae9a9a663bf964955e2746d14c2
User <User@WIN-H77MA6T5FBT>
2026-01-11 14:53:38 +0500

    URL

---
+++ p511-oop/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p511_oop {
+    internal class Class1 {
+    }
+}
