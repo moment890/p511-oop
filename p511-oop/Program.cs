17084a8e1dab9203a4edf8bb1cec6cd679727a46
User <User@WIN-H77MA6T5FBT>
2026-01-25 13:06:17 +0500

    yjdsq

--- p511-oop/Program.cs
+++ p511-oop/Program.cs
@@ -7,31 +7,12 @@ using System.Text.Json;
 using System.Net.Http;
 
 namespace p511_oop {
-    public class Person {
-        public string Name { get; set; }
-        public int Age { get; set; }
-    }
-
+   
     internal class Program {
-        static void Main(string[] args) {
-           Person person = new Person {
-               Name = "John",
-                Age = 30
-            };
+        static void Main(string[] args)
+            {
+           
+            }
 
-            // Создание JSON
-           string json = JsonSerializer.Serialize(person);
-            Console.WriteLine(json);
-
-           // Преобразование из JSON в объект
-            Person deserialized = JsonSerializer.Deserialize<Person>(json);
-            const string URL = "https://jsonplaceholder.org/posts";
-
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri(URL);
-
-            var response = client.GetAsync(URL);
-            Console.WriteLine(response.Result);
-        }
     }
 }
